e9e87548a0d1ab140d8cf642013c761ac9e546fc
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-06-07 10:10:51 +0000

    Split server into lib and bin

---
+++ server/src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+pub extern crate lazy_static;
+pub extern crate actix;
+pub extern crate actix_web;
+pub extern crate chrono;
+pub extern crate dotenv;
+pub extern crate jsonwebtoken;
+//pub extern crate lettre;
+//pub extern crate lettre_email;
+//pub extern crate rand;
+//pub extern crate regex;
+//pub extern crate rss;
+pub extern crate serde;
+pub extern crate serde_json;
+
+pub mod settings;
